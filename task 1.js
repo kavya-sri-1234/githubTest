593c1d96feeb08738a81ab83c7b1e7cfbef080e3
kavya-sri-1234 <90023133+kavya-sri-1234@users.noreply.github.com>
2021-11-26 19:52:08 +0530

    Add files via upload

--- task 1.js
+++ task 1.js
@@ -1,29 +1,13 @@
-var Person1 = new Object()
-Person1.FirstName = "KAVYA",
-Person1.LastName = "SREE",
-Person1.OfficeAddress = "NTR NAGAR,KOVVUR"
+function person(FirstName ,LastName,Office_Address){
+    this.FirstName=FirstName,
+    this.LastName=LastName,
+    this.Office_Address=Office_Address
+}
 
-var Person2 = new Object()
-Person2.FirstName = "MYTHILY",
-Person2.LastName = "SREE SAI",
-Person2.OfficeAddress = "BHAGYA NAGAR,KADAPA"
+var p1 = new person("KAVYA","SREE","Ameerpet , Hyderabad");
+var p2 = new person("MYTHILY","SREE SAI","Kukatpally , Hyderabad");
+var p3 = new person("RAJESH","KUMAR","Gachibowli,Hyderabad");
 
-var Person3 = new Object()
-Person3.FirstName = "RAJESH",
-Person3.LastName = "KUMAR",
-Person3.OfficeAddress = "RK NAGAR,VIZAG"
-
-console.log("Person1:")
-console.log("FirstName: " + Person1.FirstName)
-console.log("LastName: " + Person1.LastName)
-console.log("OfficeAddress: " + Person1.OfficeAddress)
-
-console.log("Person2:")
-console.log("FirstName: " + Person2.FirstName)
-console.log("LastName: " + Person2.LastName)
-console.log("OfficeAddress: " + Person2.OfficeAddress)
-
-console.log("Person3:")
-console.log("FirstName: " + Person3.FirstName)
-console.log("LastName: " + Person3.LastName)
-console.log("OfficeAddress: " + Person3.OfficeAddress)
+console.log(p1);
+console.log(p2);
+console.log(p3);
