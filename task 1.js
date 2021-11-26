7806ca4fb90336a465805ac3976c432d4bd5abbd
kavya-sri-1234 <90023133+kavya-sri-1234@users.noreply.github.com>
2021-11-26 19:37:18 +0530

    Add files via upload

--- task 1.js
+++ task 1.js
@@ -1,12 +1,29 @@
-function person(FirstName ,LastName,Office_Address){
-    this.FirstName=FirstName,
-    this.LastName=LastName,
-    this.Office_Address=Office_Address
-}
-var p1 = new person("kavya","chowdary","london , losangels,1111");
-var p2 = new person("rajesh","kumar","hyderabad,1111");
-var p3 = new person("sri","chowdary","patna,1111");
+var Person1 = new Object()
+Person1.FirstName = "PF1",
+Person1.LastName = "PL1",
+Person1.OfficeAddress = "POA1"
 
-console.log(p1);
-console.log(p2);
-console.log(p3);
+var Person2 = new Object()
+Person2.FirstName = "PF2",
+Person2.LastName = "PL2",
+Person2.OfficeAddress = "POA2"
+
+var Person3 = new Object()
+Person3.FirstName = "PF3",
+Person3.LastName = "PL3",
+Person3.OfficeAddress = "POA3"
+
+console.log("Person1:")
+console.log("FirstName: " + Person1.FirstName)
+console.log("LastName: " + Person1.LastName)
+console.log("OfficeAddress: " + Person1.OfficeAddress)
+
+console.log("Person2:")
+console.log("FirstName: " + Person2.FirstName)
+console.log("LastName: " + Person2.LastName)
+console.log("OfficeAddress: " + Person2.OfficeAddress)
+
+console.log("Person3:")
+console.log("FirstName: " + Person3.FirstName)
+console.log("LastName: " + Person3.LastName)
+console.log("OfficeAddress: " + Person3.OfficeAddress)
